82e54fa2084ea4ae139c74e5b4afc1f612474f87
nonowar7 <sabalanoam77@gmail.com>
2023-05-01 17:49:10 +0300

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod run;
 
 // comments
 // tests
-// wiki data
-// plotters -> avg value and max value
-// check results
-// add option for threads during training
-// handle cases of "chunks" in data for counting
-// includes
-// config default arguments check
+// wiki data lisence
+// plotters -> avg value and max value                  => done
+// check results                                        => done
+// includes and erase redundant code, pub vs pri
+// uninstall what not used, organize toml
+// config default arguments check                       => done
+// option to result when possible
+// existence of keys and values hash
 
 fn main() {
     run::Run::run();
@@ -20,3 +21,10 @@ fn main() {
 
 // file preprocess:
 // strip, remove empty lines, to lower, remove headlines (only articles), add SOS EOS
+// counting is done on lines, sequences of texts, that can be as large as the string buffer allows.
+// hence the input should be file of sequences and not one huge chunk of text.
+// counting is done in slices of size and zipped to save. In training slices that fit variable 32 are 
+// loaded once (as many io calls as slices and not a call for every time we process as slice), so RAM
+// should allow it. Training is done in one thread.
+// results on 100M look good.
+// plotting uses max and mean, should be replaced with PCA
